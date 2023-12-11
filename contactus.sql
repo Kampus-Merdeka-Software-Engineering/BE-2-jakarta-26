20d1398ecef47ca7d0a9764d13bec0a27cebac1f
andiennathania <149322195+andiennathania@users.noreply.github.com>
2023-12-11 14:54:14 +0700

    contactus.sql

--- contactus.sql
+++ contactus.sql
@@ -4,7 +4,6 @@ USE NewsPortal;
 CREATE TABLE IF NOT EXISTS ContactUs (
     contact_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     message TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
